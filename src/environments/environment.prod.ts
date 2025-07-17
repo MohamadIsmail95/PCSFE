e2e2cfaf2d5266276513fda7cddcf469b9503a8c
Eng.Mohammad <Eng.Mohammad@MHDISM>
2025-07-17 11:49:27 +0300

    change production dns

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   //apiUrl: 'https://localhost:44343/api/',
-  apiUrl: 'https://192.168.5.139:4001/api/',
+  apiUrl: 'https://tmbackend:8080/api/',
   production: true,
   test: false,
   baseHref: '/',
